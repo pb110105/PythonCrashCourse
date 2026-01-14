8fb8edb32ff63f6a156a2a0968a2424bfbdcd8cb
pb110105 <phamtranquocbao110105@gmail.com>
2026-01-14 18:29:31 +0700

    Update more lesson

---
+++ 6/Exercise.py
@@ -0,0 +1,37 @@
+#6-1. Person
+person = {
+    'first_name': 'Anh',
+    'last_name': 'Nguyen',
+    'age': 17,
+    'city': 'Bien Hoa'
+}
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
+#6-2. Favorite Numbers
+favorite_numbers = {
+    'Hung': 7,
+    'Anh': 8,
+    'Bao': 1,
+    'Nha': 3,
+    'Nhi': 45
+}
+print("Hung: " + str(favorite_numbers['Hung']))
+print("Anh: " + str(favorite_numbers['Anh']))
+print("Bao: " + str(favorite_numbers['Bao']))
+print("Nha: "+ str(favorite_numbers['Nha']))
+print("Nhi: " + str(favorite_numbers['Nhi']))
+#6-3. Glossary
+glossary = {
+    'string': 'A series of characters.',
+    'list': 'A collection of times in a particular order.',
+    'dictionary': 'A collection of key-value pairs',
+    'loop': 'A way to repeat code multiple times.',
+    'function': 'A block of code that performs a specific task.'
+}
+print("string: " + glossary['string'])
+print("list: " + glossary['list'])
+print("dictionary: " + glossary['dictionary'])
+print("loop: " + glossary['loop'])
+print("function: " + glossary['function'])
--- 6/favorite_languages.py
+++ 6/favorite_languages.py
@@ -7,3 +7,20 @@ favorite_languages = {
 }
 language = favorite_languages['sarah'].title()
 print(f"Sarah's favorite language is {language}")
+for n, l in favorite_languages.items():
+    print(f"{n.title()}'s favorite language is {l.title()}")
+#Looping Through All the Keys in a Dictionary
+for name in favorite_languages.keys():
+    print(name.title())
+for name in favorite_languages.values():
+    print(name.title())
+for name in favorite_languages:
+    print(name.title())
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f"Hi {name.title()}")
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}")
+if 'erin' not in favorite_languages.keys():
+    print("Erin, please take our poll!")
