cf377f2035eefe34ff5621e74e1fe391f11031a8
pb110105 <phamtranquocbao110105@gmail.com>
2026-02-01 23:34:36 +0700

    Update Chapter 9

--- 9/exercise.py
+++ 9/exercise.py
@@ -35,3 +35,53 @@ User2.greet_user()
 User3 = User("Anh", "Pham")
 User3.describe_user()
 User3.greet_user()
+#9-4. Number Served
+class restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    def describe_restaurant(self):
+        print(f"You choose {self.restaurant_name} and {self.cuisine_type}")
+    def open_restaurant(self):
+        print(f"The {self.restaurant_name} is opening")
+    def set_number_served(self):
+        self.number_served = 25
+        print(f"Number of customers served: {self.number_served}")
+    def increment_number_served(self, additional_customers):
+        self.number_served += additional_customers
+        print(f"Number of customers served: {self.number_served}")
+my_restaurant = restaurant("Dookie", "Buffet")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
+my_restaurant.set_number_served()
+my_restaurant.increment_number_served(30)
+#9-5. Login Attempts
+class User:
+    def __init__(self, first_name, last_name, login_attempts):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = login_attempts
+    def describe_user(self):
+        print(f"{self.first_name} {self.last_name}")
+    def greet_user(self):
+        print(f"Hello {self.first_name} {self.last_name}")
+    def increment_login_attempts(self):
+        self.login_attempts = 0
+        self.login_attempts += 1
+        print(f"Login attempts: {self.login_attempts}")
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print(f"Login attempts reset to: {self.login_attempts}")
+
+User1 = User("Bao", "Pham", 0)
+User1.describe_user()
+User1.greet_user()
+User1.increment_login_attempts()
+User1.reset_login_attempts()
+User2 = User("Anh", "Nguyen", 0)
+User2.describe_user()
+User2.greet_user()
+User3 = User("Anh", "Pham", 0)
+User3.describe_user()
+User3.greet_user()
