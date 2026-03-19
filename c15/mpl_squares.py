0c13a1b113507cdf917a77b4a3069c7b7752baf3
pb110105 <phamtranquocbao110105@gmail.com>
2026-03-19 16:13:06 +0700

    Update chapter 15

---
+++ c15/mpl_squares.py
@@ -0,0 +1,16 @@
+#Plotting a Simple line graph
+import matplotlib.pyplot as plt
+input_values = [1,2,3,4,5]
+squares = [1,4,9,16,25]
+# Use a style that is available in this Matplotlib version
+# (e.g., seaborn-v0_8 instead of plain 'seaborn')
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(input_values, squares, linewidth = 3)
+#Changing the Label Type and Line Thickness
+ax.set_title("Square Numbers", fontsize = 24)
+ax.set_xlabel("Value", fontsize = 14)
+ax.set_ylabel("Square of Value", fontsize = 14)
+ax.tick_params(labelsize = 14)
+plt.show()
+
